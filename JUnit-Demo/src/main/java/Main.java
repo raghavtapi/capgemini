a004e31641a09b724e6c21f901c26cc975611875
raghavtapi <56125409+raghavtapi@users.noreply.github.com>
2020-01-31 15:45:24 +0530

    Add files via upload

---
+++ JUnit-Demo/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		emp.setId(101);
+		emp.setName("Raghav");
+		System.out.println(emp.getId()+" : "+emp.getName());
+	}
+
+}
---
+++ JUnit-Demo/src/test/java/SampleTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void simpleTesPass() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+
+	@Test
+	public void simpleTestFail() {
+		int a = 2;
+		int b = 2;
+		assertTrue(a + b == 4);
+	}
+}
